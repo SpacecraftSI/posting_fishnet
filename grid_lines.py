fcb6f9fe5d0bf36107ca55996b4c8153c65040f4
Leh <lehsmallshaw@gmail.com>
2023-03-16 19:21:00 -0700

    grid lines v2 working better than v1.. v1 can probably be removed. Still kinks to work out in v2

--- grid_lines.py
+++ grid_lines.py
@@ -2,10 +2,7 @@
 # takes 'raw' ais line segments and breaks them into gridded cells and attributes each new or existing line with a grid id.
 # to do this it also recalculates the speed and duration (based of proportion of line)
 
-# steps
-#
 
-import logging
 from datetime import datetime
 import psycopg2 as pg
 
@@ -19,144 +16,27 @@ conn = pg.connect(host=auth_class.login.host,
                   password=auth_class.login.pw,
                   options='-c search_path=dbo,' + str(auth_class.login.schem))  # sets schema to public
 
+# this could be moved into auth class really or just do away with auth class all together is the other option
+landtable = 'akbcwaor'
+
+# filters
+sog = 87
+length = 10000
+duration = 21600
+
 
 
 def main():
-    global conn
 
     start_time = datetime.now()
 
-    datelist = []
-    datelist = datefinder(conn)
-    print(datelist)
-
-    # not going to loop through dates yet while i'm testing
-    temper(conn) # might not even need this and do away with the temp table?? --> might be useful to keep for testing, though.
-
-    the_intersector(conn)
-
-    len_sog(conn)
-
-    # select duplicate row id's and make a sub-id with decimal
-    data_caboose(conn)
-
-    # STILL NEED TO DUMP TEMP INTO FINAL TABLE
-    data_finalizer(conn)
-
-    # probably don't need this but this closes out the cursor (and saves changes)- probably just good practice
-    conn.commit()
-    conn.close()
-
-    now = datetime.now()
-    duration = (now - start_time)
-
-    print(duration)
-
-def the_intersector(conn):
-    # moves the selected data from the main database into the temp database
+    #drops output table if the name already exists
     cursor = conn.cursor()
-
-    sql = (
-    'INSERT INTO ' + auth_class.login.tempDb + '(segmentid, uid, mmsi, starttime, duration, isclassa, classais, classgen, name, isunique, lastchange, lenm, sogkt, inter, id1km, id2km, id4km, id8km) ' +
-    'SELECT l.segmentid, l.uid, l.mmsi, l.starttime, l.duration, l.isclassa, l.classais, l.classgen, l.name, l.isunique, l.lastchange, l.lenm, l.sogkt, ' +
-    'ST_INTERSECTION(l.geom, c.geom) AS inter, c.id_1km AS id1km, c.id_2km as id2km, c.id_4km as id4km, c.id_8km as id8km ' +
-    'FROM ' + auth_class.login.inputDb + ' AS l, ' + auth_class.login.gridDb + ' AS c WHERE ST_INTERSECTS(l.geom, c.geom)'
-    )
-
+    sql = "DROP TABLE IF EXISTS " + auth_class.login.outputDb
     cursor.execute(sql)
 
-def len_sog(conn):
-    # recalculates the length of each segment (now that some are cut) and recalculates the duration accordingly based on sog and length
-    cursor = conn.cursor()
-
-    # recalculates the length--> a couple things to note. This calculates CARTESIAN by default AND the units based on the srid (currently 3005).
-    # use: ST_Distance_Sphere to calculate spherical if desired
-    sql = 'UPDATE ' + auth_class.login.tempDb + ' SET lenm = ST_LENGTH(inter)'
-    cursor.execute(sql)
-
-    # recalculates the duration of the segment based on the newly calculated length and the originally calculated speed over ground
-    # the equation for this is: lenm [length in meters] / (sog[kts]*0.514) = duration [in seconds]
-    sql = 'UPDATE ' + auth_class.login.tempDb + ' SET duration = (lenm / (sogkt*0.514))'
-    cursor.execute(sql)
-
-
-def temper(conn):
-    # this functionality checks if a temp folder already exists and drops the existing one if it does -- regardless the script will create a blank temp folder
-    exists = False
-    try:
-        cursor = conn.cursor()
-        cursor.execute("select exists(select relname from pg_class where relname='" + auth_class.login.tempDb + "')")
-        # turns exists to True if a temp table already exists in the database
-        exists = cursor.fetchone()[0]
-    finally:
-        pass
-
-    # deletes existing temp table if exists is TRUE
-    if exists == True:
-        cursor = conn.cursor()
-        sql = 'DROP TABLE ' + auth_class.login.tempDb
-        cursor.execute(sql)
-    else:
-        pass
-
-    # create temp table in database
-    cursor = conn.cursor()
-    sql =   ('CREATE TABLE ' + auth_class.login.tempDb + ' ' +
-            '(newid VARCHAR(50),' +
-            'segmentId BIGINT,' +
-            'uid BIGINT NOT NULL,' +
-            'mmsi INT NOT NULL,' +
-            'startTime TIMESTAMP WITHOUT TIME ZONE NOT NULL,' +
-            'duration INT NOT NULL,' +
-            'isClassA BOOL NOT NULL,' +
-            'classAIS SMALLINT NOT NULL,' +
-            'classGen SMALLINT NOT NULL,' +
-            'name VARCHAR(20),' +
-            'isUnique BOOL NOT NULL,' +
-            'lastChange TIMESTAMP WITHOUT TIME ZONE NOT NULL,' +
-            'lenM FLOAT,' +
-            'sogKt FLOAT,' +
-            'inter GEOMETRY(LineString,3005),' +
-            'id1km VARCHAR(50),' +
-            'id2km VARCHAR(50),' +
-            'id4km VARCHAR(50),' +
-            'id8km VARCHAR(50))'
-             )
-
-    cursor.execute(sql)
-
-
-def datefinder(conn):
-    # select dates that occur within the segments being worked on. This keeps the temp smaller and compartmentalizes the work a little bit
-    cursor = conn.cursor()
-    # sql = "SELECT starttime FROM " + auth_class.login.inputDb + " WHERE segmentid IN ({})".format(str(segList)[1:-1])
-    sql = "SELECT starttime FROM " + auth_class.login.inputDb
-    cursor.execute(sql)
-    result = cursor.fetchall()
-
-    # loop to convert datetime to date in a list
-    datelist = []
-    x = 0
-    for i in result:
-        datelist.append(result[x][0].strftime("'%Y-%m-%d'"))
-        x += 1
-
-    # only keeps unique dates to prevent unnecessary loops
-    datelist = set(datelist)
-
-    return datelist
-
-
-def data_caboose(conn):
-    # this function adds the associated cwsid1km (smallest resolution) to the segment id which will result in a totally unique id. Downside is that it is varchar.. so might be worth redoing this later.
-    cursor = conn.cursor()
-    sql = "UPDATE " + auth_class.login.tempDb + " SET newid = (segmentid || id1km)"
-    cursor.execute(sql)
-
-def data_finalizer(conn):
-    cursor = conn.cursor()
-    # check if table exists and if not creates a new output table
-    sql = ('CREATE TABLE IF NOT EXISTS ' + auth_class.login.outputDb +
+    # creates new output table
+    sql = ('CREATE TABLE ' + auth_class.login.outputDb +
            ' (newid VARCHAR(50),' +
            'segmentId BIGINT,' +
            'uid BIGINT NOT NULL,' +
@@ -180,11 +60,142 @@ def data_finalizer(conn):
            )
     cursor.execute(sql)
 
-    # inserts data from the temporary table into the new main table -- does an upsert that skips on conflict with NEWID
-    sql = ("INSERT INTO " + auth_class.login.outputDb + " (newid,segmentid,uid,mmsi,starttime,duration,isclassa,classais,classgen,name,isunique,lastchange,lenm,sogkt,inter,id1km,id2km,id4km,id8km)" +
-        " SELECT newid,segmentid,uid,mmsi,starttime,duration,isclassa,classais,classgen,name,isunique,lastchange,lenm,sogkt,inter,id1km,id2km,id4km,id8km FROM " + auth_class.login.tempDb +
-        " ON CONFLICT (newid) DO NOTHING")
+    # creates of list of all unique dates in the dataset for looping- hopefully deals with memory issues.
+    print("TEST 1")
+    sql = "SELECT starttime FROM " + auth_class.login.inputDb
     cursor.execute(sql)
+    result = cursor.fetchall()
+    dateList = []
+    x = 0
+    for i in result:
+        dateList.append(result[x][0].strftime("'%Y-%m-%d'"))
+        x += 1
+    # only keeps unique dates to prevent unnecessary loops
+    dateList = set(dateList)
+
+
+
+    for date in dateList:
+        sub_time = datetime.now()
+
+        print("TEST 2")
+        filterer(conn, date)
+
+        #the_intersector(conn)
+
+        #len_sog(conn)
+
+        # select duplicate row id's and make a sub-id with decimal
+        #id_caboose(conn)
+
+        # probably don't need this but this closes out the cursor (and saves changes)- probably just good practice
+        conn.commit()
+
+        current_time = datetime.now()
+        sub_duration = (current_time - sub_time)
+        print(str(date) + " took " + str(sub_duration) + " to processes.")
+
+    conn.close()
+
+    # calcs total processing time
+    now = datetime.now()
+    duration = (now - start_time)
+    print("Total processing time: " + str(duration))
+
+def the_intersector(conn):
+    # moves the selected data from the main database into the temp database
+    cursor = conn.cursor()
+
+    # makes sure an up-to-date index exists to keep things speedy
+    sql = ('DROP INDEX IF EXISTS idx')
+    cursor.execute(sql)
+    sql = ('CREATE INDEX idx ON temp USING gist (geom)')
+    cursor.execute(sql)
+
+    sql = (
+    'INSERT INTO ' + auth_class.login.outputDb + '(segmentid, uid, mmsi, starttime, duration, isclassa, classais, classgen, name, isunique, lastchange, lenm, sogkt, inter, id1km, id2km, id4km, id8km) ' +
+    'SELECT l.segmentid, l.uid, l.mmsi, l.starttime, l.duration, l.isclassa, l.classais, l.classgen, l.name, l.isunique, l.lastchange, l.lenm, l.sogkt, ' +
+    'ST_INTERSECTION(l.geom, c.geom) AS inter, c.id_1km AS id1km, c.id_2km as id2km, c.id_4km as id4km, c.id_8km as id8km ' +
+    'FROM ' + auth_class.login.tempDb + ' AS l, ' + auth_class.login.gridDb + ' AS c WHERE ST_INTERSECTS(l.geom, c.geom) ON CONFLICT (newid) DO NOTHING'
+    )
+
+    cursor.execute(sql)
+
+
+def len_sog(conn):
+    # recalculates the length of each segment (now that some are cut) and recalculates the duration accordingly based on sog and length
+    cursor = conn.cursor()
+
+    # recalculates the length--> a couple things to note. This calculates CARTESIAN by default AND the units based on the srid (currently 3005).
+    # use: ST_Distance_Sphere to calculate spherical if desired
+    sql = 'UPDATE ' + auth_class.login.outputDb + ' SET lenm = ST_LENGTH(inter)'
+    cursor.execute(sql)
+
+    # recalculates the duration of the segment based on the newly calculated length and the originally calculated speed over ground
+    # the equation for this is: lenm [length in meters] / (sog[kts]*0.514) = duration [in seconds]
+    sql = 'UPDATE ' + auth_class.login.outputDb + ' SET duration = (lenm / (sogkt*0.514))'
+    cursor.execute(sql)
+
+
+def filterer(conn, date):
+    cursor = conn.cursor()
+    sql = "DROP TABLE IF EXISTS " + auth_class.login.tempDb
+    cursor.execute(sql)
+
+    # create temp table in database
+    """
+    sql = ('CREATE TABLE ' + auth_class.login.tempDb + ' ' +
+           '(newid VARCHAR(50),' +
+           'segmentId BIGINT,' +
+           'uid BIGINT NOT NULL,' +
+           'mmsi INT NOT NULL,' +
+           'startTime TIMESTAMP WITHOUT TIME ZONE NOT NULL,' +
+           'duration INT NOT NULL,' +
+           'isClassA BOOL NOT NULL,' +
+           'classAIS SMALLINT NOT NULL,' +
+           'classGen SMALLINT NOT NULL,' +
+           'name VARCHAR(20),' +
+           'isUnique BOOL NOT NULL,' +
+           'lastChange TIMESTAMP WITHOUT TIME ZONE NOT NULL,' +
+           'lenM FLOAT,' +
+           'sogKt FLOAT,' +
+           'inter GEOMETRY(LineString,3005),' +
+           'id1km VARCHAR(50),' +
+           'id2km VARCHAR(50),' +
+           'id4km VARCHAR(50),' +
+           'id8km VARCHAR(50))'
+           )
+
+    cursor.execute(sql)
+    """
+
+    # selects filtered data both geographically, and statistically, into the temp. Reduces the amount of data that needs to be processed for the intersection increasing performance.
+    sql = ("SELECT * INTO "+auth_class.login.tempDb+" FROM " + auth_class.login.inputDb + " WHERE CAST(starttime AS DATE)  = " + date + " AND sogkt < "+str(sog)+" and lenm < "+str(length)+" and duration < "+str(duration))
+    cursor.execute(sql)
+    print("statistical filter compete")
+
+    conn.commit()
+
+    #sql = ("SELECT a.segmentid FROM temps AS a, akbcwaor AS b WHERE st_intersects(a.geom,b.geom)")
+    #cursor.execute(sql)
+    """
+    landids = [r[0] for r in cursor.fetchall()]
+    landids = tuple(landids)
+    print(len(landids))
+    print("land select complete")
+
+    sql = ("DELETE FROM " + auth_class.login.tempDb + " WHERE segmentid IN %s")
+    cursor.execute(sql, (landids,))
+    print("land selected segments deleted from table")
+    """
+
+
+def id_caboose(conn):
+    # this function adds the associated cwsid1km (smallest resolution) to the segment id which will result in a totally unique id. Downside is that it is varchar.. so might be worth redoing this later.
+    cursor = conn.cursor()
+    sql = "UPDATE " + auth_class.login.outputDb + " SET newid = (segmentid || id1km)"
+    cursor.execute(sql)
+
 
 if __name__ == ('__main__'):
     main()
---
+++ grid_lines_v2.py
@@ -0,0 +1,196 @@
+# grid_lines_v2.py
+# takes 'raw' ais line segments and breaks them into gridded cells and attributes each new or existing line with a grid id.
+# to do this it also recalculates the speed and duration (based of proportion of line)
+# this is stealing from filterer as that methodology seems to do better
+
+from datetime import datetime
+import psycopg2 as pg
+
+import auth_class
+
+# establish database connection
+conn = pg.connect(host=auth_class.login.host,
+                  port=auth_class.login.port,
+                  dbname=auth_class.login.db,
+                  user=auth_class.login.user,
+                  password=auth_class.login.pw,
+                  options='-c search_path=dbo,' + str(auth_class.login.schem))  # sets schema to public
+
+# this could be moved into auth class really or just do away with auth class all together is the other option
+landtable = 'akbcwaor'
+
+# filters
+sog = 87
+length = 10000
+duration = 21600
+
+# gets rid of any features overlapping this polygon
+landshape = "akbcwaor"
+
+gridjoin = True
+outgrid = ((auth_class.login.outputDb)+"gridagg")
+
+
+def main():
+    start_time = datetime.now()
+    print("Creating: " + auth_class.login.outputDb)
+
+
+    output_table()
+    the_filter()
+    the_intersector()
+    length_calc()
+    unique_id()
+
+    if gridjoin == True:
+        print("Creating: " + outgrid)
+        aisclass = grid_table()
+        grid_join(aisclass)
+
+
+    conn.commit()
+
+
+    now = datetime.now()
+    duration = (now - start_time)
+    print("Completed in: " + str(duration))
+
+def grid_join(aisclass):
+    cursor = conn.cursor()
+    for ais in aisclass:
+        sql = ("UPDATE " + outgrid + " SET count" + str(ais) + " = newtab.counts, len" + str(ais) + " = newtab.len, elap" + str(ais) + " = newtab.elap, avg" + str(ais) + " = newtab.avg FROM " +
+                "(SELECT ves.classgen, grid.id_1km, COUNT(DISTINCT ves.uid) AS counts, SUM(ves.lenm) AS len, SUM(ves.duration) AS elap, AVG(ves.sogkt) AS avg " +
+                "FROM " + outgrid + " AS grid " +
+                "JOIN " + auth_class.login.outputDb + " AS ves " +
+                "ON grid.id_1km = ves.id1km " +
+                "GROUP BY grid.id_1km, ves.classgen) AS newtab " +
+                "WHERE newtab.classgen = " + str(ais) + " AND newtab.id_1km = "+outgrid+".id_1km")
+        cursor.execute(sql)
+
+######### NOT WORKING ################
+    sql = "UPDATE " + outgrid + " SET countagg = (count0+count1+count2+count3+count4+count5+count6+count7+count8)"
+    cursor.execute(sql)
+    sql = "UPDATE " + outgrid + " SET lenagg = (len0+len1+len2+len3+len4+len5+len6+len7+len8)"
+    cursor.execute(sql)
+    sql = "UPDATE " + outgrid + " SET elapagg = (elap0+elap1+elap2+elap3+elap4+elap5+elap6+elap7+elap8)"
+    cursor.execute(sql)
+
+
+### THIS CAN BE SIMPLIFIED -- ALSO NOT WORKING ##
+    sql = ("UPDATE " + outgrid + " SET avgagg = newtab.avg FROM " +
+            "(SELECT grid.id_1km, AVG(ves.sogkt) as avg " +
+            "FROM " + outgrid + " AS grid " +
+            "JOIN " + auth_class.login.outputDb + " AS ves " +
+            "ON grid.id_1km = ves.id1km " +
+            "GROUP BY grid.id_1km) AS newtab " +
+            "WHERE newtab.id_1km = "+outgrid+".id_1km")
+    cursor.execute(sql)
+
+
+def grid_table():
+    cursor = conn.cursor()
+    sql = 'DROP TABLE IF EXISTS ' + outgrid
+    cursor.execute(sql)
+
+    sql = ("SELECT * INTO " + outgrid + " FROM " + auth_class.login.gridDb)
+    cursor.execute(sql)
+
+    aisclass = (0,1,2,3,4,5,6,7,8)
+    columnnames = [['count', 'INT'], ['len','FLOAT'],['elap','FLOAT'], ['avg', 'FLOAT']]
+    for columns in columnnames:
+        sql = "ALTER TABLE " + outgrid + " ADD COLUMN " + columns[0] + "agg " + columns[1]
+        cursor.execute(sql)
+        for ais in aisclass:
+            sql = "ALTER TABLE " + outgrid + " ADD COLUMN " + columns[0]+str(ais) + " " + columns[1]
+            cursor.execute(sql)
+    return aisclass
+
+
+def unique_id():
+    # this function adds the associated cwsid1km (smallest resolution) to the segment id which will result in a totally unique id. Downside is that it is varchar.. so might be worth redoing this later.
+    cursor = conn.cursor()
+    sql = "UPDATE " + auth_class.login.outputDb + " SET newid = (segmentid || id1km)"
+    cursor.execute(sql)
+
+def length_calc():
+    # recalculates the length of each segment (now that some are cut) and recalculates the duration accordingly based on sog and length
+    cursor = conn.cursor()
+
+    # recalculates the length--> a couple things to note. This calculates CARTESIAN by default AND the units based on the srid (currently 3005).
+    # use: ST_Distance_Sphere to calculate spherical if desired
+    sql = 'UPDATE ' + auth_class.login.outputDb + ' SET lenm = ST_LENGTH(inter)'
+    cursor.execute(sql)
+
+    # recalculates the duration of the segment based on the newly calculated length and the originally calculated speed over ground
+    # the equation for this is: lenm [length in meters] / (sog[kts]*0.514) = duration [in seconds]
+    sql = 'UPDATE ' + auth_class.login.outputDb + ' SET duration = (lenm / (sogkt*0.514))'
+
+    cursor.execute(sql)
+
+def the_intersector():
+    cursor = conn.cursor()
+    sql = (
+    'INSERT INTO ' + auth_class.login.outputDb + '(segmentid, uid, mmsi, starttime, duration, isclassa, classais, classgen, name, isunique, lastchange, lenm, sogkt, inter, id1km, id2km, id4km, id8km) ' +
+    'SELECT l.segmentid, l.uid, l.mmsi, l.starttime, l.duration, l.isclassa, l.classais, l.classgen, l.name, l.isunique, l.lastchange, l.lenm, l.sogkt, ' +
+    'ST_INTERSECTION(l.geom, c.geom) AS inter, c.id_1km AS id1km, c.id_2km as id2km, c.id_4km as id4km, c.id_8km as id8km ' +
+    'FROM ' + auth_class.login.tempDb + ' AS l, ' + auth_class.login.gridDb + ' AS c WHERE ST_INTERSECTS(l.geom, c.geom) ON CONFLICT (newid) DO NOTHING')
+
+    cursor.execute(sql)
+
+def the_filter():
+    cursor = conn.cursor()
+    # drops existing temporary table
+    sql = 'DROP TABLE IF EXISTS ' + auth_class.login.tempDb
+    cursor.execute(sql)
+
+    # moves filtered data into a temporary table
+    sql = ("SELECT * INTO "+auth_class.login.tempDb+" FROM " + auth_class.login.inputDb + " WHERE sogkt < " + str(sog) + " AND lenm < " + str(length) + " AND duration < " + str(duration) + " AND duration > 0")
+    cursor.execute(sql)
+
+    # creates index --> it's a relatively light task and doing it every time just adds some bulletproofness
+    sql = ('DROP INDEX IF EXISTS idx')
+    cursor.execute(sql)
+    sql = ("CREATE INDEX idx ON "+auth_class.login.tempDb+" USING gist (geom)")
+    cursor.execute(sql)
+
+    sql = "DROP TABLE IF EXISTS tempsland"
+    cursor.execute(sql)
+
+    sql = ("SELECT a.segmentid INTO tempsland from "+auth_class.login.tempDb+" as a, "+landtable+" as b where st_intersects(a.geom,b.geom)")
+    cursor.execute(sql)
+    sql = ("DELETE FROM "+auth_class.login.tempDb+" USING tempsland WHERE "+auth_class.login.tempDb+".segmentid = tempsland.segmentid")
+    cursor.execute(sql)
+
+
+def output_table():
+    #drops output table if the name already exists
+    cursor = conn.cursor()
+    sql = "DROP TABLE IF EXISTS " + auth_class.login.outputDb
+    cursor.execute(sql)
+    # creates new output table
+    sql = ('CREATE TABLE ' + auth_class.login.outputDb +
+           ' (newid VARCHAR(50),' +
+           'segmentId BIGINT,' +
+           'uid BIGINT NOT NULL,' +
+           'mmsi INT NOT NULL,' +
+           'startTime TIMESTAMP WITHOUT TIME ZONE NOT NULL,' +
+           'duration INT NOT NULL,' +
+           'isClassA BOOL NOT NULL,' +
+           'classAIS SMALLINT NOT NULL,' +
+           'classGen SMALLINT NOT NULL,' +
+           'name VARCHAR(20),' +
+           'isUnique BOOL NOT NULL,' +
+           'lastChange TIMESTAMP WITHOUT TIME ZONE NOT NULL,' +
+           'lenM FLOAT,' +
+           'sogKt FLOAT,' +
+           'inter GEOMETRY(LineString,3005),' +
+           'id1km VARCHAR(50),' +
+           'id2km VARCHAR(50),' +
+           'id4km VARCHAR(50),' +
+           'id8km VARCHAR(50),'
+           'UNIQUE(newid))'
+           )
+    cursor.execute(sql)
+
+if __name__ == ('__main__'):
+    main()
